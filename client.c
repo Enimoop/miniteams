10d21beaab461dc85428a8bd96edc9e449e64539
MForcioli <marineforcioli@gmail.com>
2024-02-04 18:00:49 +0100

    Ajout de pause et nettoyage github

--- client.c
+++ client.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 int recu;
 
@@ -13,71 +13,55 @@ void send_end_text(int pid);
 void send_end_pid(int pid);
 void recevoir(int x);
 
-int main(int argc, char* argv[])
-{  
+int main(int argc, char* argv[]){
     if(argc==3)
     {
         recu = 0;
         int pid = atoi(argv[1]);
         char cli[7];
-        sprintf(cli,"%d",getpid());
-        printf("%i\n", getpid());
+        sprintf(cli, "%d", getpid());
+        printf("%i\n",getpid());
         char* mes = argv[2];
 
-        if (strlen(mes)>850){
-            printf("Ecrivez un message inférieur a 850 caractères\n");
-        }
-        else{
-        printf("%i\n",pid);
+        
+        printf("%i\n",pid);       
         printf("%s\n",mes);
         convert_bin(cli,pid);
         send_end_pid(pid);
         recu = 2;
         convert_bin(mes,pid);
         send_end_text(pid);
-        }
+        
         return 0;
-    }
-    else
-    {
-        printf("Not enough arguments\n");
+    } else {
+        printf("Erreur: nombre d'arguments incorrect\n");
     }
 }
 
-
-void convert_bin(char* mes, int pid)
-{
-    int i=0;
-    while(mes[i]!='\0')
-    {
-       
+void convert_bin(char* mes, int pid){
+    int i = 0;
+    while(mes[i]!='\0') {
         char hex = mes[i];
         int size = 8;
 
-        while(size>=1)
-        {
-            size--;
-            int b = ((hex & (1<<size))!=0);
-            send_signal(b,pid);
-
+        while(size>=1) {
+           size --;
+           int b =((hex & (1<<size))!=0);
+           send_signal(b,pid);
         }
 
-        if(recu != 0) {
+        if (recu != 0) {
             signal(SIGUSR1, recevoir);
-            while(recu != 2)
-            {
+            while(recu != 2) {
                 pause();
             }
             recu = 2;
         }
         i++;
     }
-
-
 }
 
-void send_end_text(int pid)
-{
+void send_end_text(int pid){
     send_signal(0,pid);
     send_signal(0,pid);
     send_signal(0,pid);
@@ -88,8 +72,7 @@ void send_end_text(int pid)
     send_signal(1,pid);
 }
 
-void send_end_pid(int pid)
-{
+void send_end_pid(int pid){
     send_signal(0,pid);
     send_signal(0,pid);
     send_signal(0,pid);
@@ -100,30 +83,19 @@ void send_end_pid(int pid)
     send_signal(0,pid);
 }
 
-void recevoir (int x)
-{
-
-    if (x == SIGUSR1)
-    {
-    usleep(10000000);
+void recevoir(int x){
+    if(x == SIGUSR1) {
+        
         recu = 1;
     }
 }
 
-void send_signal(int b,int pid)
-{
-    if(b==0)
-    {
-        kill(pid,SIGUSR1);
-       
-           
+void send_signal(int b, int pid) {
+    if (b == 0) {
+        usleep(100);
+        kill(pid, SIGUSR1);
+    } else if (b == 1) {
+        usleep(100);
+        kill(pid, SIGUSR2);
     }
-    else if(b==1)
-    {
-        kill(pid,SIGUSR2);
-       
-       
-    }
-   
-
 }
--- server.c
+++ server.c
@@ -24,45 +24,45 @@ char* detect_language(char* message) {
 
     const double avg_frequencies[NUM_LANGUAGES][NUM_LETTERS] = {
     // Français
-    {8.13, 0.94, 3.26, 3.67, 17.21, 1.06, 1.27, 0.92, 7.34, 0.31, 0.05, 6.01, 2.96, 7.13, 5.26, 3.01, 0.99, 6.55, 8.08, 7.07, 5.74, 1.32, 0.04, 0.45, 0.3, 0.12},
+    {7.636, 0.901, 3.260, 3.669, 14.715, 1.066, 0.866, 0.737, 7.529, 0.613, 0.074, 5.456, 2.968, 7.095, 5.796, 2.521, 1.362, 6.693, 7.948, 7.244, 6.311, 1.838, 0.049, 0.427, 0.128, 0.326},
     // Anglais
-    {8.17, 1.49, 2.78, 4.25, 12.7, 2.23, 2.02, 6.09, 6.97, 0.15, 0.77, 4.03, 2.41, 6.75, 7.51, 1.93, 0.09, 6.04, 6.86, 9.37, 3.26, 1.34, 0.06, 1.06, 0.27, 0.05},
+    {8.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 6.094, 6.966, 0.153, 0.772, 4.025, 2.406, 6.749, 7.507, 1.929, 0.095, 5.987, 6.327, 9.056, 2.758, 0.978, 2.360, 0.150, 1.974, 0.074},
     // Allemand
-    {6.35, 1.65, 3.03, 5.11, 16.87, 1.49, 3.06, 4.17, 7.27, 0.27, 1.21, 3.44, 2.53, 9.78, 2.59, 0.67, 0.02, 6.89, 6.42, 7.77, 4.11, 0.89, 1.98, 0.04, 0.02, 0.03},
+    {6.516, 1.886, 3.062, 5.076, 17.396, 1.656, 3.009, 4.577, 7.550, 0.268, 1.217, 3.437, 2.534, 9.776, 2.514, 0.790, 0.018, 7.003, 7.270, 6.154, 4.346, 0.846, 1.891, 0.034, 0.039, 1.134},
     // Espagnol
-    {11.53, 2.22, 4.02, 5.07, 12.61, 0.69, 1.01, 0.7, 6.25, 0.49, 0.01, 4.97, 3.16, 6.71, 8.68, 2.51, 0.88, 6.87, 7.98, 8.67, 4.85, 1.14, 0.08, 0.23, 1.01, 0.47}
+    {12.525, 1.415, 4.679, 5.810, 13.681, 0.692, 1.008, 0.703, 6.247, 0.493, 0.011, 4.967, 3.157, 6.712, 8.683, 2.510, 0.877, 6.871, 7.977, 4.632, 2.927, 0.898, 0.017, 0.215, 0.898, 0.467}
 };
 
 
     int letter_counts[NUM_LETTERS] = {0};
-    int total_letters = 0;
+    
 
     for (int i = 0; message[i] != '\0'; i++) {
         char c = message[i];
         if (isalpha(c)) {
             letter_counts[tolower(c) - 'a']++;
-            total_letters++;
+            
         }
     }
 
     double letter_frequencies[NUM_LETTERS] = {0.0};
-    for (int i = 0; i < 26; i++) {
-        letter_frequencies[i] = (double)letter_counts[i] / total_letters * 100.0;
+    for (int i = 0; i < NUM_LETTERS; i++) {
+        letter_frequencies[i] = (double)letter_counts[i] / strlen(message) * 100.0;
     }
 
-    double min_diff = INFINITY;
+    double min_diff =100 * NUM_LETTERS;
     int detected_language = -1;
 
-    for (int i = 0; i < 26; i++) {
+    for (int i = 0; i < NUM_LETTERS; i++) {
         double diff = 0.0;
-        for (int j = 0; j < 26; j++) {
+        for (int j = 0; j < NUM_LANGUAGES; j++) {
             diff += fabs(letter_frequencies[j] - avg_frequencies[i][j]);
         }
 
-        if (diff < min_diff) {
-            min_diff = diff;
-            detected_language = i;
-        }
+         if (diff < min_diff) {
+                min_diff = diff;
+                detected_language = i;
+            }
     }
 
     switch (detected_language) {
@@ -119,7 +119,10 @@ void stock_message(char** message, char c)
     (*message)[ind] = c;
     ind++;
 
-   
+    if(ind == current_size) {
+        current_size *= 2;
+        *message = realloc(*message, current_size);
+    }
 
 
     if (*message == NULL) {
@@ -156,6 +159,7 @@ char convert_bin_to_char(char* bin_mes)
 
 void send_signal(int b, int pid) {
     if (b == 1) {
+        usleep(100);
         kill(pid, SIGUSR1);
     }
 }
@@ -231,7 +235,7 @@ int main(void)
 
     while(1)
     {
-    usleep(10000);
+    
         pause();
     }
 }
